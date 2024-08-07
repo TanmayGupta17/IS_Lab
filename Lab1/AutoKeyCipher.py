b2bd9703ee78254057e6149ffccadfa405ece485
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2024-08-07 22:17:48 +0530

    Update AutoKeyCipher.py

--- Lab1/AutoKeyCipher.py
+++ Lab1/AutoKeyCipher.py
@@ -1,22 +1,55 @@
 str = "the house is being sold tonight"
-key = chr(ord('a')+7-1)
 
-newkey = key+str
+key = "g"
+
 res = []
 shifted = []
 count1 = 0
 count2 = 0
+
 for i in str:
     if i.isalpha():
-        if count1 < len(key):
+        if count1<len(key):
             res.append(key[count1])
-            count+=1
-            shifted.append(str[i])
-        elif
-newkey = newkey[:len(str)]
-print(newkey)
+            count1 += 1
+            shifted.append(i)
+        elif count2 < len(str):
+            res.append(shifted[count2])
+            count2 += 1
+            shifted.append(i)
+    else:
+        res.append(i)
+print("newKey: "+"".join(res))
+print("Plaintext: "+str)
 
 cipher = ""
-print(cipher)
-# newKey = t+str
-# print(newKey)
+
+for i in range(len(str)):
+    c1 = str[i]
+    c2 = res[i]
+    if(c1.isalpha()):
+        char = chr((ord(c1)-ord('a')+ord(c2)-ord('a'))%26+ord('a'))
+        cipher += char
+    else:
+        cipher += c1
+print("Encrypted: "+cipher)
+
+
+decrypted_text = ""
+key_for_decryption = key  # Start with the keyword
+
+i=0
+j=0
+while i < len(cipher):
+    c1 = cipher[i]
+    if c1.isalpha():
+        c2 = key_for_decryption[j]
+        char = chr((ord(c1)-ord('a') - (ord(c2)-(ord('a')))+26) % 26 + ord('a'))
+        decrypted_text += char
+        key_for_decryption += char
+        j += 1
+    else:
+        decrypted_text += c1
+    i+=1
+
+print("Decryption:", decrypted_text)
